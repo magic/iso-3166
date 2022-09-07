27441e90fe2e30ef12606ef8703324b2ffa12791
jaeh <jascha@jaeh.at>
2022-09-08 01:46:47 +0200

    test/subdivisions: remove console.log

--- test/subdivisions.mjs
+++ test/subdivisions.mjs
@@ -4,26 +4,23 @@ import { countries, subdivisions } from '../src/index.mjs'
 
 const mLog = (i, sub) => i === 0 && console.log(sub)
 
-const tests = subdivisions.map(
-  (sub, i) =>
-    mLog(i, sub) || [
-      { fn: sub.name, expect: is.str, info: `.name of ${sub.name} is a string` },
-      { fn: sub.type, expect: is.str, info: `.type of ${sub.name} is a string` },
-      { fn: sub.code, expect: is.str, info: `.code of ${sub.name} is a string` },
-      {
-        fn: sub.code,
-        expect: t => t.length >= 1 && t.length <= 3,
-        info: `.code of ${sub.name} is between 2 and 3 characters long: ${sub.code}`,
-      },
-      { fn: sub.country, expect: is.str, info: `.country of ${sub.name} is a string` },
-      { fn: sub.country, expect: is.len.eq(2), info: `.country of ${sub.name} has length 2` },
-      { fn: sub.country, expect: is.len.eq(2), info: `.country of ${sub.name} has length 2` },
-      {
-        fn: countries.find(c => c.a2 === sub.country),
-        expect: is.objectNative,
-        info: `${sub.name} has a country with a2: ${sub.country}`,
-      },
-    ],
-)
+const tests = subdivisions.map(sub => [
+  { fn: sub.name, expect: is.str, info: `.name of ${sub.name} is a string` },
+  { fn: sub.type, expect: is.str, info: `.type of ${sub.name} is a string` },
+  { fn: sub.code, expect: is.str, info: `.code of ${sub.name} is a string` },
+  {
+    fn: sub.code,
+    expect: t => t.length >= 1 && t.length <= 3,
+    info: `.code of ${sub.name} is between 2 and 3 characters long: ${sub.code}`,
+  },
+  { fn: sub.country, expect: is.str, info: `.country of ${sub.name} is a string` },
+  { fn: sub.country, expect: is.len.eq(2), info: `.country of ${sub.name} has length 2` },
+  { fn: sub.country, expect: is.len.eq(2), info: `.country of ${sub.name} has length 2` },
+  {
+    fn: countries.find(c => c.a2 === sub.country),
+    expect: is.objectNative,
+    info: `${sub.name} has a country with a2: ${sub.country}`,
+  },
+])
 
 export default deep.flatten(tests)
