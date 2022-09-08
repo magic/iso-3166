33b82e924501bb94c1c8d820fd65a8f1d3e12ff0
jaeh <jascha@jaeh.at>
2022-09-08 22:57:56 +0200

    add docs

---
+++ docsrc/app.mjs
@@ -0,0 +1,13 @@
+export const state = {
+  title: '@magic/mime-types',
+  description: [
+    'mime-db mimetypes that have file extensions.',
+    'exports a key => value store, where key = extension and value = mime/type',
+  ],
+  logotext: '@magic/mime-types',
+  menu: [
+    { to: '/#install', text: 'install' },
+    { to: '/#usage', text: 'usage' },
+    { to: '/#source', text: 'source' },
+  ],
+}
